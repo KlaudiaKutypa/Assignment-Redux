d4bce1e6cad4a7bd73f738a7af763b59c08e6d0e
KlaudiaKutypa <klaudia.kutypa@gmail.com>
2022-03-16 17:16:25 +0100

    developingpart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+
+  const [all, setAll] = useState([]);
+  const API = "http://localhost:3001/pictures";
+
+  const handleSeePictures = () => {
+    fetch(`${API}`)
+    .then((response) => response.json())
+    .then((data) => setAll(data))
+  }
+
+  useEffect(() => {
+    handleSeePictures();
+  },[]);
+
+  const [counter, setCounter] = useState(0);
+  
+  const handleClick = (id) => {
+    setCounter(prevCounter => prevCounter +1);
+    console.log(id)
+    // if (id == id) {
+    //   setCounter(prevCounter => prevCounter +1)
+    //   console.log(id)
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className ="App__box">
+      {all.map ((picture) =>(
+        <div className="App__box__pic" key={picture.id}>
+         <img src={picture.pic}></img>
+          <button onClick={() => {handleClick(picture.id)}}>{picture.status}{counter}</button>
+          <button>NIE OK</button>
+      
+        </div>
+        
+        ))}
+      
+      </div>
     </div>
   );
 }
